db60f92f2f90fffe0582b87a84aa7032158e69c4
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-05-25 18:08:29 -0300

    On My Way!

---
+++ RD-001.py
@@ -0,0 +1,12 @@
+from math import sqrt, floor
+
+print("----------------------------")
+
+number = float(input("Digite um número: "))
+square = sqrt(number)
+
+print("----------------------------")
+print("A raiz quadrada do número digitado é {0}".format(floor(square)))
+print("----------------------------")
+print("Encerrando...")
+print("----------------------------")
---
+++ RD-002.py
@@ -0,0 +1,7 @@
+import emoji
+
+print("----------------------------")
+
+print(emoji.emojize("Olá, mundo :earth_americas:", use_aliases=True))
+
+print("----------------------------")
