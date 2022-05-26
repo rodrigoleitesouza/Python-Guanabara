36c004653ab845400abd6b75e34a486af6799293
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-05-26 18:41:43 -0300

    Create EX-030.py

---
+++ exercises/021-030/EX-030.py
@@ -0,0 +1,16 @@
+print("--------------------------")
+print("------ CONDICIONAIS ------")
+print("--------------------------")
+
+number = int(input("Digite um número inteiro -> "))
+
+print("--------------------------")
+
+if (number % 2 == 0):
+    print("O número {0} é par.".format(number))
+else:
+    print("O número {0} é ímpar.".format(number))
+
+print("--------------------------")
+print("Encerrando...")
+print("--------------------------")
