4ff9623f59f0cdaec2f0605c6c00d133172020b3
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-06-02 16:32:10 -0300

    Create EX-065.py

---
+++ exercises/061-070/EX-065.py
@@ -0,0 +1,35 @@
+import os
+
+print("---------------------------------------------------")
+
+exit = ""
+count = 0
+sum = 0
+biggestNumber = 0
+lowestNumber = 999**999
+
+while(exit != "S"):
+    count = count + 1
+    number = float(input("{0}º Número: -> ".format(count)))
+    print("---------------------------------------------------")
+    sum = sum + number
+    mean = sum/count
+
+    if (biggestNumber < number):
+        biggestNumber = number
+
+    if (lowestNumber > number):
+        lowestNumber = number
+
+    exit = str(input("Gostaria de encerrar? [S/N] -> ")).upper()
+    os.system('cls')
+
+print("---------------------------------------------------")
+
+print("Média: {0:.2f}".format(mean))
+print("Maior: {0}".format(biggestNumber))
+print("Menor: {0}".format(lowestNumber))
+
+print("---------------------------------------------------")
+print("Encerrando...")
+print("---------------------------------------------------")
