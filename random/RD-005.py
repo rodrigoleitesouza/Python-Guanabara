24756d70acb0c1af566bb98144280b70290e135b
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-06-04 11:26:36 -0300

    Create RD-005.py

---
+++ random/RD-005.py
@@ -0,0 +1,21 @@
+print("---------------------------------------------------")
+
+lanche = ("Hamburguer", "Pizza", "Pudim", "Sorvete", "Picolé", "Amendoim")
+
+print(sorted(lanche))
+
+print("---------------------------------------------------")
+
+for count in range(0, len(lanche)):
+    print(lanche[count])
+
+print("---------------------------------------------------")
+print("FIM")
+
+# for food in lanche:
+#    print(food)
+# print("FIM")
+
+print("---------------------------------------------------")
+print("Encerrando...")
+print("---------------------------------------------------")
