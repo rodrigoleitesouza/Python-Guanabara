725f704e796faedd43d197bb613f79904f6bdd9b
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-06-04 17:26:57 -0300

    Update EX-075.py

--- exercises/071-080/EX-075.py
+++ exercises/071-080/EX-075.py
@@ -1,41 +1,56 @@
-from random import randrange
-
 print("---------------------------------------------------")
 
-tuplaRandomNumbers = (
-    randrange(100),
-    randrange(100),
-    randrange(100),
-    randrange(100),
-    randrange(100),
+numberA = int(input("Digite um número: "))
+numberB = int(input("Digite um número: "))
+numberC = int(input("Digite um número: "))
+numberD = int(input("Digite um número: "))
+
+sum = 0
+count = 0
+
+tupleNumbers = (
+    numberA,
+    numberB,
+    numberC,
+    numberD,
 )
 
-print(f"A tupla com números aleatórios é: {tuplaRandomNumbers}")
+print("---------------------------------------------------")
+
+print(f"Você digitou os números: {tupleNumbers}")
 
 print("---------------------------------------------------")
 
-biggestNumber = 0
-lowestNumber = 999
-count = 0
+print(f"O número 9 apareceu {tupleNumbers.count(9)} vez(es).")
 
-for count in range(0, len(tuplaRandomNumbers), +1):
-    if (biggestNumber < tuplaRandomNumbers[count]):
-        biggestNumber = tuplaRandomNumbers[count]
-    count = count + 1
-
-count = 0
-
-for count in range(0, len(tuplaRandomNumbers), +1):
-    if (lowestNumber > tuplaRandomNumbers[count]):
-        lowestNumber = tuplaRandomNumbers[count]
-    count = count + 1
-
-print(f"Maior: {biggestNumber}")
+if (tupleNumbers.count(3) > 0):
+    for count in range(0, len(tupleNumbers), +1):
+        if (tupleNumbers[count] == 3):
+            print("---------------------------------------------------")
+            print(
+                f"O 1º número 3 apareceu na posição {tupleNumbers.index(3)+1}.")
+            break
+else:
+    print("---------------------------------------------------")
+    print("Não existe nenhum número 3 na tupla.")
 
 print("---------------------------------------------------")
 
-print(f"Menor: {lowestNumber}")
+for count in range(0, len(tupleNumbers), +1):
+    if (tupleNumbers[count] % 2 == 0):
+        print("Os números pares digitados são -> ", end="")
+        break
 
+for count in range(0, len(tupleNumbers), +1):
+    if (tupleNumbers[count] % 2 == 0):
+        sum = sum + 1
+        print(f"{tupleNumbers[count]}... ", end="")
+
+if (sum == 0):
+    print("Nenhum número par foi digitado.")
+
+
+print("")
 print("---------------------------------------------------")
 print("Encerrando...")
 print("---------------------------------------------------")
