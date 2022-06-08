27c228efa4ecfaf93808509c218b77e8b9d65ebd
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-06-08 20:25:20 -0300

    Create EX-092.py

---
+++ exercises/081-090/EX-092.py
@@ -0,0 +1,39 @@
+from datetime import date
+
+print("---------------------------------------------------")
+
+person = {}
+
+currentYear = (date.today().year)
+
+while(True):
+    person["name"] = str(input("Nome: "))
+
+    birthYear = int(input("Qual o ano de nascimento: "))
+
+    person["yearsOld"] = (currentYear - birthYear)
+
+    person["workNumber"] = int(input("Carteira de Trabalho (0 não tem): "))
+
+    if (person["workNumber"] == 0):
+        print("---------------------------------------------------")
+        for key, value in person.items():
+            print(f"{key} -> {value}")
+        break
+
+    person["jobStarted"] = int(input("Ano de contratação: "))
+
+    person["income"] = float(input("Salário (R$): "))
+
+    person["retirement"] = ((person["jobStarted"] - birthYear) + 35)
+
+    break
+
+print("---------------------------------------------------")
+
+for key, value in person.items():
+    print(f"{key} -> {value}")
+
+print("---------------------------------------------------")
+print("Encerrando...")
+print("---------------------------------------------------")
