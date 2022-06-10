dc6d1a1d0af8746872f529dcea22460abf25764e
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-06-09 21:42:22 -0300

    Update EX-095.py

--- exercises/081-090/EX-095.py
+++ exercises/081-090/EX-095.py
@@ -1,11 +1,8 @@
-
-
 team = list()
 
 player = dict()
 
 games = list()
-
 
 while (True):
     player.clear()
@@ -13,14 +10,16 @@ while (True):
 
     player["name"] = str(input("Qual o nome do jogador? -> "))
 
-    totalGamesPlayed = int(input(f'Quantas partidas {player["name"]} jogou? -> '))
+    totalGamesPlayed = int(
+        input(f'Quantas partidas {player["name"]} jogou? -> '))
 
     games.clear()
 
     print("---------------------------------------------------")
 
     for count in range(0, totalGamesPlayed):
-        games.append(int(input(f'Quantos gols o {player["name"]} marcou na partida {count+1}? -> ')))
+        games.append(
+            int(input(f'Quantos gols o {player["name"]} marcou na partida {count+1}? -> ')))
 
     player["goals"] = games[:]
     player["totalGoals"] = sum(games)
@@ -29,7 +28,8 @@ while (True):
 
     while (True):
         print("---------------------------------------------------")
-        exit = str(input("Gostaria de adicionar mais um jogador? [S/N] -> ")).upper()[0]
+        exit = str(
+            input("Gostaria de adicionar mais um jogador? [S/N] -> ")).upper()[0]
         if exit in "SN":
             break
         if exit not in "SN":
@@ -40,11 +40,17 @@ while (True):
 
 print("---------------------------------------------------")
 print()
+print()
+print()
+print()
+print("---------------------------------------------------")
 print("cód.  ", end="")
 
 for index in player.keys():
     print(f'{index:<15}', end="")
 print()
+
+print("---------------------------------------------------")
 
 for key, value in enumerate(team):
     print(f'{key:>3}  ', end="")
@@ -52,6 +58,20 @@ for key, value in enumerate(team):
         print(f'{str(data):<15}', end="")
     print()
 
-print("---------------------------------------------------")
+while (True):
+    print("---------------------------------------------------")
+    search = int(input("Mostrar dados de qual jogador? (999 para parar) -> "))
+    print("---------------------------------------------------")
+
+    if (search == 999):
+        break
+    if (search >= len(team)):
+        print("ERROR! Valor inválido, tente novamente.")
+    else:
+        print(f"DADOS DO JOGADOR -> {team[search]['name']}")
+        print("---------------------------------------------------")
+        for index, goals in enumerate(team[search]['goals']):
+            print(f' -> Jogo {index+1}: {goals} gol(s)')
+
 print("Encerrando...")
 print("---------------------------------------------------")
