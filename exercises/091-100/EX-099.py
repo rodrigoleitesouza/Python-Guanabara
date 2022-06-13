676308e3d2ecff31b04710ce1b4ba168da5885fd
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-06-13 16:37:25 -0300

    Update EX-099.py

--- exercises/091-100/EX-099.py
+++ exercises/091-100/EX-099.py
@@ -2,6 +2,7 @@ def higherNumber(*numbers):
     biggestNumber = 0
     count = 0
 
+    print("---------------------------------------------------")
     print("Analisando os valores passados...")
     print("---------------------------------------------------")
 
@@ -13,15 +14,20 @@ def higherNumber(*numbers):
     print(f"Total de números: {len(numbers)}")
     print("---------------------------------------------------")
 
-    for count in range(0, len(numbers), +1):   
+    for count in range(0, len(numbers), +1):
         if (numbers[count] > biggestNumber):
             biggestNumber = numbers[count]
         count = count + 1
     print(f'O maior número é {biggestNumber}.')
 
-print("---------------------------------------------------")
+
+# real code
 
 higherNumber(5, 3, 8, 1)
+higherNumber(6, 33, 88, 91)
+higherNumber(53, 33, 18, 12)
+higherNumber(75, 73, 18, 199)
+higherNumber(57, 73, 81, 91)
 
 print("---------------------------------------------------")
 print("Encerrando...")
