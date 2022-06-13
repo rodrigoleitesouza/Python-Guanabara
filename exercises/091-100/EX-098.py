61b35f11115858f624b15e9715b1da6542e2e706
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-06-13 11:38:57 -0300

    Create EX-098.py

---
+++ exercises/091-100/EX-098.py
@@ -0,0 +1,42 @@
+from time import sleep
+
+def count(start, end, increment):
+    if (increment < 0):
+        increment = increment*(-1)
+
+    if (increment == 0):
+        increment = 1
+
+    if (start < end):
+        for start in range(start, end+1, +increment):
+            print(f'{start}... ', end="")
+            sleep(0.3)
+        print()
+
+    if (start > end):   
+        for start in range(start, end-1, -increment):
+            print(f'{start}... ', end="")
+            sleep(0.3)
+        print()
+
+print("---------------------------------------------------")
+
+start = int(input("Início: "))
+end = int(input("Fim: "))
+increment = int(input("Incremento: "))
+
+print("---------------------------------------------------")
+
+count(1, 10, 1)
+
+print("---------------------------------------------------")
+
+count(10, 0, 2)
+
+print("---------------------------------------------------")
+
+count(start, end, increment)
+
+print("---------------------------------------------------")
+print("Encerrando...")
+print("---------------------------------------------------")
