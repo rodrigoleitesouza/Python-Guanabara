4e8f2362e194da88474b621987aa098fda96eafe
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-06-13 11:37:52 -0300

    Create RD-008.py

---
+++ random/RD-008.py
@@ -0,0 +1,19 @@
+print("---------------------------------------------------")
+
+def doubleList(list):
+    count = 0
+    while (count < len(list)):
+        list[count] = list[count] * 2
+        count = count + 1
+
+listOfNumbers = [2, 4, 5, 77]
+
+print(listOfNumbers)
+
+doubleList(listOfNumbers)
+
+print(listOfNumbers)
+
+print("---------------------------------------------------")
+print("Encerrando...")
+print("---------------------------------------------------")
