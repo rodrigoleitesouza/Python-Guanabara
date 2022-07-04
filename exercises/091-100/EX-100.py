24d5a9d43c427ceaed535fb37b3a77ce6fe1591f
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-07-04 16:59:59 -0300

    Update EX-100.py

--- exercises/091-100/EX-100.py
+++ exercises/091-100/EX-100.py
@@ -1,9 +1,11 @@
 from random import randint
 
+
 def randomize(list):
     for count in range(0, 5, +1):
         number = randint(1, 10)
         list.append(number)
+
 
 def sumEven(list):
     sumEvenNumbers = 0
@@ -28,6 +30,7 @@ print(f'A lista dos números sorteados é -> {listOfNumbers}')
 print("---------------------------------------------------")
 
 print(f'A soma dos números pares da lista acima é -> {result}')
+
 
 print("---------------------------------------------------")
 print("Encerrando...")
